3986e965bbdced3f3cc86f4e7573e52aa61354f7
hiluyx <luyx27877@163.com>
2021-09-20 23:06:28 +0800

    (fix) 漏

--- utils/tabChange.js
+++ utils/tabChange.js
@@ -6,32 +6,28 @@ var app = getApp();
 function bindTabChange(item) {
   // 当index不为0时，弹出登录申请。
   if (item.index !== 0) {
-    sessionOnLogin();
-  }
-}
-
-function sessionOnLogin() {
-  wx.checkSession({
-    success: (res) => {
-      // 登录会话中
-    },
-    fail: function () {
-      // 非登录会话中
-      wx.showModal({
-        title: '提示',
-        content: '会话过期，请重新授权',
-        success: res => {
-          if (res.confirm) {
-            onLogin();
-          } else if (res.cancel) {
-            wx.reLaunch({ // 跳转回index
-              url: '/pages/index/index'
-            })
+    wx.checkSession({
+      success: (res) => {
+        // 登录会话中
+      },
+      fail: function () {
+        // 非登录会话中
+        wx.showModal({
+          title: '提示',
+          content: '会话过期，请重新授权',
+          success: res => {
+            if (res.confirm) {
+              onLogin();
+            } else if (res.cancel) {
+              wx.reLaunch({ // 跳转回index
+                url: '/pages/index/index'
+              })
+            }
           }
-        }
-      })
-    }
-  })
+        })
+      }
+    })
+  }
 }
 
 /**
@@ -47,8 +43,19 @@ function onLogin() {
           success: function(resUserInfo) {
             // 得到用户的信息
             sendData['userInfo'] = resUserInfo.userInfo;
+            wx.setStorageSync('userInfo', resUserInfo.userInfo)
             sendData['code'] = res.code;
-            serverSessionRenew(sendData);
+            wx.request({
+              // 发送用户信息给后端更新，顺便刷新redis session
+              url: app.globalData.serverDomain + '/self/login',
+              data: sendData,
+              success: function(res) {
+                // res 为返回的数据
+              },
+              fail: function (res) {
+          
+              }
+            })
           },
           fail: function() {
             return;
@@ -58,21 +65,6 @@ function onLogin() {
     },
 
     fail: function(res) {
-
-    }
-  })
-}
-
-function serverSessionRenew(sendData) {
-  console.log(sendData);
-  wx.request({
-    // 发送用户信息给后端更新，顺便刷新redis session
-    url: app.globalData.serverDomain + '/self/login',
-    data: sendData,
-    success: function(res) {
-      // res 为返回的数据
-    },
-    fail: function (res) {
 
     }
   })
